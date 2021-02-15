cd127a05955f092dea6c2b6d92024cd9a95e2570
Pratikshya Regmi <61108147+Pratikshya-Regmi@users.noreply.github.com>
2021-02-15 17:59:41 +0545

    Update image_classification.js

--- image_classification.js
+++ image_classification.js
@@ -8,7 +8,7 @@ Map.addLayer(image, {bands: ['B4', 'B3', 'B2'],min:0, max: 3000}, 'True colour i
 ////merge
 var newfc = urban.merge(vegetation);
 print(newfc);
-//create training data
+// create training data
 var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
 var training = image.select(bands).sampleRegions({
   collection: newfc,
@@ -16,13 +16,13 @@ var training = image.select(bands).sampleRegions({
   scale: 30
 });
 print(training);
-///train classifier
+/// train classifier
 var classifier = ee.Classifier.cart().train({
   features: training,
   classProperty: 'landcover',
   inputProperties: bands
 });
-//Run the classification
+// Run the classification
 var classified = image.select(bands).classify(classifier);
 //Display classification
 Map.centerObject(newfc, 11);
